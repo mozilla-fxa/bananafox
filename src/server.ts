60994cb0185a9e5f44a3a3c32ae8a6f6ecadfdc2
Danny Coates <dannycoates@gmail.com>
2020-09-01 14:29:39 -0700

    goofin

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,53 @@
-// import child_process from 'child_process';
-import probot, { Application } from 'probot'
-const { Probot } = probot
-import { updateYarnLock } from './yarn.js'
+import type Webhooks from "@octokit/webhooks";
+import probot, { Application } from "probot";
+const { Probot } = probot;
+import { updateYarnLock } from "./yarn.js";
 
 export const bot = (app: Application) => {
-  app.on('pull_request.opened', async (context) => {
-    const pr = context.payload.pull_request
-    const owner = pr.head.repo.owner.login
-    const repo = pr.head.repo.name
-    context.github.issues.createComment(context.issue({body: 'hello, all!'}))
-    // if (pr.user.login === 'dependabot') {
-    const { sha, content } = await updateYarnLock(owner, repo, pr.head.sha)
-    const updateInfo = {
-      owner,
-      repo,
-      path: 'yarn.lock',
-      message: 'chore(deps): updated yarn.lock',
-      content,
-      sha,
-      branch: pr.head.ref
+  const prHook = async (
+    context: probot.Context<Webhooks.WebhookPayloadPullRequest>
+  ) => {
+    if (context.payload.sender.login === 'fxa-bananafox[bot]') {
+      return
     }
-    app.log({...updateInfo, content: updateInfo.content.length})
-    await context.github.repos.createOrUpdateFile(updateInfo)
-  })
-}
+    const pr = context.payload.pull_request;
+    const owner = pr.head.repo.owner.login;
+    const repo = pr.head.repo.name;
+
+    const { sha, content } = await updateYarnLock(owner, repo, pr.head.sha);
+    if (content) {
+      const updateInfo = {
+        owner,
+        repo,
+        path: "yarn.lock",
+        message: "chore(deps): updated yarn.lock",
+        content,
+        sha,
+        branch: pr.head.ref,
+      };
+      app.log({
+        ...updateInfo,
+        content: updateInfo.content.length,
+        message: undefined,
+      });
+      await context.github.repos.createOrUpdateFile(updateInfo);
+      context.github.issues.createComment(
+        context.issue({
+          body: `I got u ${context.payload.sender.login}. 💯`,
+        })
+      );
+    } else if (pr.user.login.includes("dependabot")) {
+      context.github.issues.createComment(
+        context.issue({
+          body: `I couldn't update the \`yarn.lock\`. 🍌`,
+        })
+      );
+    }
+  };
+  app.on("pull_request.opened", prHook);
+  app.on("pull_request.synchronize", prHook);
+};
 
 if (import.meta.url.includes(process.argv[1])) {
-  Probot.run(bot)
+  Probot.run(bot);
 }
--- src/yarn.ts
+++ src/yarn.ts
@@ -1,54 +1,71 @@
-import fs from 'fs'
-import child_process from 'child_process'
-import crypto from 'crypto'
-import os from 'os'
-import path from 'path'
-import { Writable, pipeline } from 'stream';
-import { promisify } from 'util'
-import tar from 'tar';
-import fetch from 'node-fetch'
+import fs from "fs";
+import child_process from "child_process";
+import os from "os";
+import path from "path";
+import tar from "tar";
+import fetch from "node-fetch";
 
-const asyncPipeline = promisify(pipeline)
+function yarnLockGitSha(cwd: string) {
+  return child_process
+    .execSync("git hash-object yarn.lock", {
+      cwd,
+      encoding: "ascii",
+    })
+    .trim();
+}
 
-export async function updateYarnLock(owner: string, repo: string, ref: string) {
-  const tmp = `${os.tmpdir()}${path.sep}${ref}`
-  await fs.promises.mkdir(tmp, { recursive: true })
-  const res = await fetch(`https://api.github.com/repos/${owner}/${repo}/tarball/${ref}`)
-  const tarStream = res.body.pipe(
+function untarDependencyFiles(
+  stream: NodeJS.ReadableStream,
+  dir: string
+): Promise<void> {
+  const tarStream = stream.pipe(
     tar.extract({
       strip: 1,
-      filter: (path) => (path.includes('package.json') || path.includes('yarn')),
-      cwd: tmp
-  }))
-  await new Promise((resolve, reject) => {
-    tarStream.once('end', resolve)
-    tarStream.once('error', reject)
-  })
-
-  const sha = child_process.execSync('git hash-object yarn.lock', {
-    cwd: tmp,
-    encoding: 'ascii'
-  })
-  // TODO use the yarn api? and not download the world
-  const proc = child_process.exec('yarn install --silent', {
-    cwd: tmp
-  })
-
-  // proc.stderr?.pipe(process.stderr)
-
-  await new Promise((resolve, reject) => {
-    // actually, we expect it to fail for now
-    // proc.once('exit', (code) => code ? reject(code) : resolve())
-    proc.once('exit', (code) => {
-      console.log('exit', code)
-      resolve()
+      filter: (path) => path.includes("package.json") || path.includes("yarn"),
+      cwd: dir,
     })
-  })
-  const newLock = fs.readFileSync(path.join(tmp, 'yarn.lock'))
+  );
+  return new Promise((resolve, reject) => {
+    tarStream.once("end", resolve);
+    tarStream.once("error", reject);
+  });
+}
+
+function yarnRelock(cwd: string): Promise<void> {
+  const proc = child_process.exec("yarn relock", {
+    cwd,
+  });
+  proc.stdout?.pipe(process.stdout);
+  proc.stderr?.pipe(process.stderr);
+
+  return new Promise((resolve, reject) => {
+    proc.once("exit", (code, signal) => {
+      code ? reject(code) : resolve();
+      signal ? reject(signal) : resolve();
+    });
+  });
+}
+
+export async function updateYarnLock(owner: string, repo: string, ref: string) {
+  const tmp = `${os.tmpdir()}${path.sep}${ref}`;
+  await fs.promises.mkdir(tmp, { recursive: true });
+  const res = await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/tarball/${ref}`
+  );
+  await untarDependencyFiles(res.body, tmp);
+  const sha = yarnLockGitSha(tmp);
+  try {
+    await yarnRelock(tmp);
+  } catch (e) {
+    console.error("yarn relock error:", e);
+  }
+  const newSha = yarnLockGitSha(tmp);
 
   return {
     sha,
-    // content: ''
-    content: newLock.toString('base64')
-  }
+    content:
+      newSha === sha
+        ? null // unchanged
+        : fs.readFileSync(path.join(tmp, "yarn.lock")).toString("base64"),
+  };
 }
